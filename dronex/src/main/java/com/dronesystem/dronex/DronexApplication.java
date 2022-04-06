91cea2385df6abc18eee341674b3bb015031e526
audrey <asuzu.audrey@sabi.am>
2022-04-06 19:24:12 +0100

    Initialized Spring-boot project with Spring web, H2 database and Spring Data JPA dependencies.

---
+++ dronex/src/main/java/com/dronesystem/dronex/DronexApplication.java
@@ -0,0 +1,13 @@
+package com.dronesystem.dronex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DronexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DronexApplication.class, args);
+	}
+
+}
---
+++ dronex/src/test/java/com/dronesystem/dronex/DronexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dronesystem.dronex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DronexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

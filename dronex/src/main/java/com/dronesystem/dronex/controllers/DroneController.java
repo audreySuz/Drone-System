e078238c2ce77c4c7a05052a7febe08957a69fe0
audrey <asuzu.audrey@sabi.am>
2022-04-22 15:30:38 +0100

    Created Endpoint to load drone with medication

--- dronex/src/main/java/com/dronesystem/dronex/controllers/DroneController.java
+++ dronex/src/main/java/com/dronesystem/dronex/controllers/DroneController.java
@@ -1,7 +1,9 @@
 package com.dronesystem.dronex.controllers;
 
+import com.dronesystem.dronex.dtos.LoadDroneRequest;
 import com.dronesystem.dronex.dtos.RegisterDroneRequest;
 import com.dronesystem.dronex.entities.Drone;
+import com.dronesystem.dronex.entities.Medication;
 import com.dronesystem.dronex.entities.Model;
 import com.dronesystem.dronex.services.DroneService;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -11,9 +13,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController; 
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
-
 
 /**
  *
@@ -22,22 +23,31 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RestController
 @RequestMapping("/drone")
-@Tag(name = "Drone",description = "Drone Controller")
+@Tag(name = "Drone", description = "Drone Controller")
 @RequiredArgsConstructor
 public class DroneController {
-    
-private final DroneService droneService;
-    
-    
+
+    private final DroneService droneService;
+
     @PostMapping("/model")
     public ResponseEntity<Model> createModel(@RequestBody Model model) {
         Model newModel = droneService.createModel(model);
         return ResponseEntity.status(HttpStatus.CREATED).body(newModel);
     }
-    
+
     @PostMapping("/drone")
-    public ResponseEntity<Drone> registerDrone(@RequestBody RegisterDroneRequest request){
-    Drone newDrone = droneService.registerDrone(request.getSerialNumber(), request.getModel());
-    return ResponseEntity.status(HttpStatus.CREATED).body(newDrone);
+    public ResponseEntity<Drone> registerDrone(@RequestBody RegisterDroneRequest request) {
+        Drone newDrone = droneService.registerDrone(request.getSerialNumber(), request.getModel());
+        return ResponseEntity.status(HttpStatus.CREATED).body(newDrone);
     }
+
+    @PutMapping("/load")
+    public ResponseEntity<Drone> loadDrone(@RequestBody LoadDroneRequest request) {
+        Drone loadedDrone = droneService.loadDrone(request.getMedication(), request.getSerialNumber());
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(loadedDrone);
+    }
+    
+    
+    
 }

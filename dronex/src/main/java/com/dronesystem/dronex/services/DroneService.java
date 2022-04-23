7283bca10242b3507ae181d491f5435f6270a7fc
audrey <asuzu.audrey@sabi.am>
2022-04-24 00:53:25 +0100

    updated method for loading drone to prevent loading the same medication twice. Updated method for loading drone, to validate code and name of medication. Created service methods to get drones available for loading, to get medications on a drone and to get battery level of a drone

--- dronex/src/main/java/com/dronesystem/dronex/services/DroneService.java
+++ dronex/src/main/java/com/dronesystem/dronex/services/DroneService.java
@@ -6,12 +6,15 @@ import com.dronesystem.dronex.entities.Model;
 import com.dronesystem.dronex.repositories.DroneRepository;
 import com.dronesystem.dronex.repositories.MedicationRepository;
 import com.dronesystem.dronex.repositories.ModelRepository;
+import com.dronesystem.dronex.utils.ValidationUtil;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.bytebuddy.asm.Advice;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  *
@@ -37,6 +40,7 @@ public class DroneService {
     public Drone registerDrone(String serialNumber, String model) {
         Model exModel = modelRepository.findByName(model);
         Drone exDrone = droneRepository.findBySerialNumber(serialNumber);
+
         if (exModel == null) {
             throw new RuntimeException("Model does not exists");
         }
@@ -53,18 +57,32 @@ public class DroneService {
         }
     }
 
-
     public Drone loadDrone(Medication medication, String serialNumber) {
+        ValidationUtil validationUtil = new ValidationUtil();
         Drone newDrone;
         Drone drone = droneRepository.findBySerialNumber(serialNumber);
+        Medication exMed = medicationRepository.findByCode(medication.getCode());
+        if (exMed!=null) {
+        throw new RuntimeException("Medication already loaded");
+        }
         int totalWeightOfMeds = drone.getMedications()
                 .stream()
                 .mapToInt(med -> med.getWeight())
                 .sum();
-        if (drone != null || drone.getDroneState() == Drone.DroneState.IDLE || drone.getDroneState() == Drone.DroneState.LOADING) {
+        if (!validationUtil.validateCode(medication.getCode())) {
+            throw new RuntimeException("Invalid code! (allowed only upper case letters, underscore and numbers)");
+        }
+        if (!validationUtil.validateMedicationName(medication.getName())) {
+            throw new RuntimeException("Invalid medication name! (allowed only letters, numbers, ‘-‘, ‘_’)");
+        }
+
+        if (drone != null || drone.getDroneState() == Drone.DroneState.IDLE
+                || drone.getDroneState() == Drone.DroneState.LOADING
+                || drone.getBatteryLevel() > 25) {
             if (totalWeightOfMeds + medication.getWeight() <= drone.getModel().getWeightLimit()) {
                 medication.setDrone(drone);
                 drone.getMedications().add(medication);
+                drone.setDroneState(Drone.DroneState.LOADING);
                 newDrone = droneRepository.save(drone);
             } else {
                 throw new RuntimeException("Medication too heavy!");
@@ -74,4 +92,33 @@ public class DroneService {
         }
         return newDrone;
     }
+
+    public List<Medication> getDroneMedications(String serialNumber) {
+        Drone drone = droneRepository.findBySerialNumber(serialNumber);
+        if (drone == null) {
+            throw new RuntimeException("Invalid Serial Number!");
+        }
+        if (drone.getDroneState() == Drone.DroneState.DELIVERED
+                || drone.getDroneState() == Drone.DroneState.DELIVERING
+                || drone.getDroneState() == Drone.DroneState.RETURNING) {
+            throw new RuntimeException("No Medications on this drone ");
+        } else {
+            return drone.getMedications();
+        }
+
+    }
+
+    public List<Drone> getAvailableDrones() {
+        List<Drone> drones = droneRepository.findByDroneStateIn(List.of(Drone.DroneState.IDLE, Drone.DroneState.LOADING));
+        return drones;
+    }
+
+    public Integer getBatteryLevel(String serialNumber){
+      Drone drone = droneRepository.findBySerialNumber(serialNumber);
+        if (drone == null) {
+            throw new RuntimeException("Invalid Serial Number!");
+        }
+        return drone.getBatteryLevel();
+    }
+    
 }
